645dc6c8779157c6ade6659e1cc13e841305f688
magicsly <bryan.eddy5@gmail.com>
2018-07-13 09:27:31 -0400

    Updated time values for machines in accordance with Process Engineering requests.

--- Data/Mes.MachineAttributes_Data.sql
+++ Data/Mes.MachineAttributes_Data.sql
@@ -1,4 +1,9 @@
 SET IDENTITY_INSERT [Mes].[MachineAttributes] ON
+INSERT INTO [Mes].[MachineAttributes] ([MachineAttributeID], [MachineID], [AttributeNameID], [DateCreated], [CreatedBy]) VALUES (32, 4, 37, '2018-06-19 14:17:57.610', N'NAA\eddybc')
+INSERT INTO [Mes].[MachineAttributes] ([MachineAttributeID], [MachineID], [AttributeNameID], [DateCreated], [CreatedBy]) VALUES (33, 5, 37, '2018-06-19 14:17:57.737', N'NAA\eddybc')
+INSERT INTO [Mes].[MachineAttributes] ([MachineAttributeID], [MachineID], [AttributeNameID], [DateCreated], [CreatedBy]) VALUES (34, 6, 37, '2018-06-19 14:17:57.787', N'NAA\eddybc')
+SET IDENTITY_INSERT [Mes].[MachineAttributes] OFF
+SET IDENTITY_INSERT [Mes].[MachineAttributes] ON
 INSERT INTO [Mes].[MachineAttributes] ([MachineAttributeID], [MachineID], [AttributeNameID], [DateCreated], [CreatedBy]) VALUES (1, 4, 3, '2018-04-23 12:56:26.210', N'NAA\eddybc')
 INSERT INTO [Mes].[MachineAttributes] ([MachineAttributeID], [MachineID], [AttributeNameID], [DateCreated], [CreatedBy]) VALUES (2, 4, 4, '2018-04-23 12:56:26.430', N'NAA\eddybc')
 INSERT INTO [Mes].[MachineAttributes] ([MachineAttributeID], [MachineID], [AttributeNameID], [DateCreated], [CreatedBy]) VALUES (3, 4, 7, '2018-04-23 12:56:26.957', N'NAA\eddybc')
